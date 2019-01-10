7bcea9a7182f1330e3af9ceb160ced62d8098c1e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-10 12:16:03 +0700

    Add check is transport work with webGL or not at transport factory

--- ENetTransportFactory.cs
+++ ENetTransportFactory.cs
@@ -2,6 +2,8 @@
 
 public class ENetTransportFactory : BaseTransportFactory
 {
+    public override bool CanUseWithWebGL { get { return false; } }
+
     public override ITransport Build()
     {
         return new ENetTransport();
