00c15423ea1cd8e50938ea6182d4fda457c9d40e
Insthync <ittipon.bay@gmail.com>
2021-10-18 20:41:29 +0700

    Update codes follow LiteNetLibManager changes

--- ENetTransportFactory.cs
+++ ENetTransportFactory.cs
@@ -2,7 +2,6 @@
 
 public class ENetTransportFactory : BaseTransportFactory
 {
-    public override bool CanUseWithWebGL { get { return false; } }
     public int maxChannel = 4;
 
     public override ITransport Build()
